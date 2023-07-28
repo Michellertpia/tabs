01fc7c6c8523f5a157a0ee9a147d40a77802431e
Michellertapia <michrod2507@gmail.com>
2023-07-28 08:35:09 -0600

    Navegacion con tabs

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, Text, View, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+import SRMain from './pantallas/SRMain';
+
+function PrincipalScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Principal</Text>
+      <Button
+        title="Ir a ajustes"
+        onPress={() => navigation.navigate('Ajustes')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AjustesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Ajustes</Text>
+      <Button
+        title="Ir a perfil"
+        onPress={() => navigation.navigate('Perfil')}
+      />
+    </View>
+  );
+}
+
+function PerfilScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Perfil</Text>
+      <Button
+        title="Ir a principal"
+        onPress={() => navigation.navigate('Principal')}
+      />
+    </View>
+  );
+}
+
+
+function MainScreen() {
+  return (
+    <SRMain/>
+  );
+}
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Principal" component={PrincipalScreen} />
+        <Tab.Screen name="Ajustes" component={AjustesScreen} />
+        <Tab.Screen name="Perfil" component={PerfilScreen} />
+        <Tab.Screen name="Producto" component={MainScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
